7a61c87eabbfb3001a3d1a318ff42eded9612ad9
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-10-22 09:03:50 +0700

    UI for Header components

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    // backgroundColor: theme.palette.background.paper,
+    boxShadow: "none",
+    // backgroundColor: '#1890ff',
+  },
+}));
+
+export default function DisabledTabs() {
+  const classes = useStyles();
+  const labels = ["Item1", "Item2", "Item3"];
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        indicatorColor="primary"
+        textColor="primary"
+        onChange={handleChange}
+        centered
+      >
+        {labels.map((label) => (
+          <Tab label={label} />
+        ))}
+      </Tabs>
+    </Paper>
+  );
+}
